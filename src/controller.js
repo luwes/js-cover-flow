c14cb9f807246eb6070214e81f29d02cc2e0eb52
Wesley Luyten <luytenwesley@gmail.com>
2013-07-13 15:31:51 +0200

    only catch horizontal touch events

--- src/controller.js
+++ src/controller.js
@@ -6,19 +6,20 @@
 		this.config = config;
 		
 		this.currentX = 0;
+		this.currentY = 0;
 		this.transformProp = Modernizr.prefixed('transitionDuration');
 	};
 
 	C.Controller.prototype.handleEvent = function(e) {
-		e.preventDefault();
 		this[e.type](e);
 	};
 	
 	C.Controller.prototype.touchstart = function(e) {
 		e.stopImmediatePropagation();
 		this.startX = e.touches[0].pageX - this.currentX;
+		this.startY = e.touches[0].pageY - this.currentY;
 		this.pageY = e.touches[0].pageY;
-		this.touchMoved = false;
+		this.moved = false;
 		window.addEventListener("touchmove", this, true);
 		window.addEventListener("touchend", this, true);
 		this.elem.style[this.transformProp] = "0s";
@@ -26,11 +27,19 @@
 	
 	C.Controller.prototype.touchmove = function(e) {
 		e.stopImmediatePropagation();
-		this.touchMoved = true;
+
 		this.lastX = this.currentX;
-		this.lastMoveTime = new Date().getTime();
+		this.lastY = this.currentY;
 		this.currentX = e.touches[0].pageX - this.startX;
-		this.flow.update(this.currentX);
+		this.currentY = e.touches[0].pageY - this.startY;
+
+		if (Math.abs(this.currentX - this.lastX) > Math.abs(this.currentY - this.lastY)) {
+			e.preventDefault();
+			this.moved = true;
+
+			this.lastMoveTime = new Date().getTime();
+			this.flow.update(this.currentX);
+		}
 	};
 	
 	C.Controller.prototype.touchend = function(e) {
@@ -40,7 +49,9 @@
 	
 		this.elem.style[this.transformProp] = this.config.tweentime + "s";
 	
-		if (this.touchMoved) {
+		if (this.moved) {
+			e.preventDefault();
+
 			var delta = this.currentX - this.lastX;
 			var dt = new Date().getTime() - this.lastMoveTime + 1;
 			
